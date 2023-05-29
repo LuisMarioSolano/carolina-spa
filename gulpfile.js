d3e8061e549371a7efaa6465c9843a54dd471870
LuisMarioSolano <luismariosga@gmail.com>
2023-05-29 10:44:58 -0600

    Install Plumber

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const { src, dest, watch, series } = require('gulp');
 
 // CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require ('gulp-plumber');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
@@ -15,6 +16,7 @@ const avif = require('gulp-avif');
 function css( done ) {
     src('src/scss/app.scss')
         .pipe( sourcemaps.init() )
+        .pipe( plumber() )
         .pipe( sass() )
         .pipe( postcss([ autoprefixer(), cssnano() ]) )
         .pipe( sourcemaps.write('.'))
